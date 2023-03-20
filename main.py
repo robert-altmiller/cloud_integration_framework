531c178bc1917e8809667dd91f448baea2583b84
Robert Altmiller <robert.altmiller@databricks.com>
2023-03-20 15:44:18 -0500

    modified text analytics module

--- main.py
+++ main.py
@@ -13,9 +13,9 @@ config = config.get_config_vars()
 def run_test(cloudservicename = None):
     """has a list of the api tests to run"""
     runtests = {
-        "azure_auth": True,
+        "azure_auth": False,
         "azure_storage": False,
-        "azure_textanalytics": False,
+        "azure_textanalytics": True,
         "aws_s3bucket": False,
         "aws_lamda": False,
         "gcp_bigquery": False
@@ -28,25 +28,31 @@ def main():
     run main program
     """
 
-    # text analytics (ta) class is in helpers --> azure_helpers.py
+    # azure text analytics keyphrases anb sentiment
+    # text analytics (ta) class is in resources --> azure_resources
     if  run_test("azure_textanalytics") == True:
         azure_ta_obj = textanalytics(config)
         ta_data_json = azure_text_analytics_api_test(azure_ta_obj)
         # write json results to a local file
+        fldr_path = config["LOCAL_DATA_FOLDER"] + "/text_analytics_example"
+        fname = "text_analytics.json"
         write_to_local(
             data = ta_data_json,
-            folderpath = config["LOCAL_DATA_FOLDER"] + "/text_analytics_example", 
-            filename = "text_analytics_json.json"
+            folderpath = fldr_path, 
+            filename = fname
         )
+        print(f"local text analytics results are stored: {fldr_path}/{fname}")
 
 
-    # s3bucket class is in helpers --> aws_helpers.py
+    # s3 bucket integration
+    # s3bucket class is in resources --> aws_resources
     if run_test("aws_s3bucket") == True:
         aws_s3_obj = s3bucket(config)
         aws_s3_bucket_api_test(aws_s3_obj)
 
     
     # azure oauth2 authentication token creation
+    # azureauth class is in resources --> azure_resources
     if run_test("azure_auth") == True:
         azure_auth_obj = azureauth(config)
         azure_auth_api_test(azure_auth_obj)
